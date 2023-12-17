ebba4c16bdc587e9c01b7522d4c5c413f7adc909
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2023-12-17 05:55:39 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ console.log("001");
 
 
 document.getElementById("goHome").addEventListener('click', () => {
-    location.href = "index.html";
+    location.href = "https://catriel-kahla.github.io/kiwinews/index.html";
 });
 
 document.getElementById("n001").addEventListener('click', () => {
-    location.href = "pages/n_001.html";
+    location.href = "https://catriel-kahla.github.io/kiwinews/pages/n_001.html";
 });
 
 
