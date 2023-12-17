33656756ea15053948ebc5b4238d74ffa2902cef
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2023-12-17 04:54:39 -0300

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -5,8 +5,5 @@ document.getElementById("goHome").addEventListener('click', () => {
     location.href = "../index.html";
 });
 
-document.getElementById("n001").addEventListener('click', () => {
-    location.href = "../pages/n_001.html";
-});
 
 
