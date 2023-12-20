f0938d85639a235a326cd98101ae0fc116149baa
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2023-12-20 02:53:35 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ console.log("001");
 
 
 document.getElementById("goHome").addEventListener('click', () => {
-    location.href = "https://catriel-kahla.github.io/kiwinews/index.html";
+    location.href = "index.html";
 });
 
 document.getElementById("n001").addEventListener('click', () => {
-    location.href = "https://catriel-kahla.github.io/kiwinews/pages/n_001.html";
+    location.href = "pages/n_001.html";
 });
 
 
--- js/news.js
+++ js/news.js
@@ -2,7 +2,7 @@ console.log("001");
 
 
 document.getElementById("goHome").addEventListener('click', () => {
-    location.href = "https://catriel-kahla.github.io/kiwinews/index.html";
+    location.href = "../index.html";
 });
 
 
