03be1e22e7ce4bafe333ed894c47462eb32e3945
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2024-03-06 00:21:36 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("001");
+
+
+document.getElementById("goHome").addEventListener('click', () => {
+    location.href = "index.html";
+});
+
+document.getElementById("njan").addEventListener('click', () => {
+    location.href = "pages/n_jan.html";
+});
+
+document.getElementById("nfeb").addEventListener('click', () => {
+    location.href = "pages/n_feb.html";
+});
+
+
