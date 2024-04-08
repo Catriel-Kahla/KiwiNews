1637cd57d3b0073062bea983566f51d82ef82129
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2024-04-08 16:44:23 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,16 +5,20 @@ document.getElementById("goHome").addEventListener('click', () => {
     location.href = "index.html";
 });
 
-document.getElementById("njan").addEventListener('click', () => {
-    location.href = "pages/n_jan.html";
-});
+if(document.getElementById("nfeb")){
+    document.getElementById("nfeb").addEventListener('click', () => {
+        location.href = "pages/n_feb.html";
+    });
+}
 
-document.getElementById("nfeb").addEventListener('click', () => {
-    location.href = "pages/n_feb.html";
-});
+if(document.getElementById("nmar")){
+    document.getElementById("nmar").addEventListener('click', () => {
+        location.href = "pages/n_mar.html";
+    });
+}
 
-document.getElementById("nmar").addEventListener('click', () => {
-    location.href = "pages/n_mar.html";
-});
+
+
+
 
 
